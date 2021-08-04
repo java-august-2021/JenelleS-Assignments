e7d64622e7e9e0e7f776776bf34d5c40e9d1c66b
Jenelle Hanson <Jlhanson9@gmail.com>
2021-08-03 21:20:24 -0500

    fizzBuzzAssignment

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz fuzzyBuziness= new FizzBuzz();
+        
+        System.out.println(fuzzyBuziness.fizzBuzz(30));
+        //fizzBuzz."method in FizzBuzz regular file"(numTo test);
+        System.out.println(fuzzyBuziness.fizzBuzz(99));
+        System.out.println(fuzzyBuziness.fizzBuzz(50));
+        System.out.println(fuzzyBuziness.fizzBuzz(79));
+    }
+}
